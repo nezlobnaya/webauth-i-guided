dbb49b36614a5e27501a263b5514db0a69d35bc3
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2020-01-13 14:40:32 -0500

    code added

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,9 +1,14 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs')
+
 
 const Users = require('../users/users-model.js');
 
 router.post('/register', (req, res) => {
   let user = req.body;
+
+  //hash the password
+ user.password = bcrypt.hashSync(user.password, 14)
 
   Users.add(user)
     .then(saved => {
@@ -17,10 +22,13 @@ router.post('/register', (req, res) => {
 router.post('/login', (req, res) => {
   let { username, password } = req.body;
 
+  //check the password
+
+
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
--- users/users-model.js
+++ users/users-model.js
@@ -8,12 +8,12 @@ module.exports = {
 };
 
 function find() {
-  return db('users').select('id', 'username');
+  return db('users').select('id', 'username', 'password');
 }
 
 function findBy(filter) {
   return db('users')
-    .select('id', 'username')
+    .select('id', 'username', 'password')
     .where(filter);
 }
 
